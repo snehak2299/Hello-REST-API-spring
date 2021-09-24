7bb82c37bbc97ddf36f21c1a1d74bbf3b2206aef
snehak2299 <skanase2299@gmail.com>
2021-09-25 00:38:07 +0530

    [sneha]: hello name where name passed as path variable

---
+++ src/main/java/com/bridglab/springrest/beans/Message.java
@@ -0,0 +1,61 @@
+package com.bridglab.springrest.beans;
+
+public class Message implements org.apache.logging.log4j.message.Message {
+	private String name;
+	private String text;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public Message(String name, String text) {
+		this.name = name;
+		this.text = text;
+	}
+
+	public Message() {
+
+	}
+
+	@Override
+	public String toString() {
+		return "Message [name=" + name + ", text=" + text + "]";
+	}
+
+	@Override
+	public String getFormattedMessage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getFormat() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object[] getParameters() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Throwable getThrowable() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- src/main/java/com/bridglab/springrest/controller/MyRestController.java
+++ src/main/java/com/bridglab/springrest/controller/MyRestController.java
@@ -1,16 +1,28 @@
 package com.bridglab.springrest.controller;
 
 
+import org.apache.logging.log4j.message.Message;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+//import com.bridglab.springrest.beans.*;
 
 @RestController
+
 public class MyRestController {
-	
+	Message msg;
 	@RequestMapping("/")
 	public String Welcome() {
 		return "Welcome to REST Page";
 	}
 
+	@RequestMapping("/hello/{name}")
+	public Message message(@PathVariable String name) {
+		msg = new com.bridglab.springrest.beans.Message(name, "Hello " + name + " !");
+		return msg;
+	}
+	
 }
 
